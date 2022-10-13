ee5c3b1d600c27915465f374c9b25662381a5a42
Pinzy <94610807+Pinzy04@users.noreply.github.com>
2022-10-13 18:02:05 +0200

    Add files via upload

---
+++ Esempio-codice/index.js
@@ -0,0 +1,21 @@
+const numero = 3;
+
+if(numero < 3)
+{
+  console.log("il numero è minore di 3");
+}
+else if(numero == 3)
+{
+  console.log("il numero è uguale a 3");
+}
+else
+{
+  console.log("il numero è maggiore a 3");
+}
+
+for(i = 0; i < 10; i++)
+  {
+    console.log(i);
+  }
+
+console.log("ciao");
