5c7c5b8a1a0ad18dfa7dee903446de11ad3c2c7a
Pinzy <94610807+Pinzy04@users.noreply.github.com>
2022-10-13 17:52:51 +0200

    Add files via upload

---
+++ frecce-direzionali/script.js
@@ -0,0 +1,13 @@
+let orizzontali = 0;
+let verticali = 0;
+
+function Verticali(n){
+  verticali = verticali + 1*n;
+  document.getElementById("vert").innerHTML = verticali;
+}
+
+function Orizzontali(n){
+  orizzontali = orizzontali + 1*n;
+  document.getElementById("oriz").innerHTML = orizzontali;
+}
+  
