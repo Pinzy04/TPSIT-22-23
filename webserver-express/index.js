f48fa15a2daad385c66aede62b5ab679ad278d91
Pinzy <94610807+Pinzy04@users.noreply.github.com>
2022-11-03 00:40:33 +0100

    Create index.js

---
+++ webserver-express/index.js
@@ -0,0 +1,38 @@
+let express = require("express");
+let app = express();
+let path = require('path');
+let utils = require('./utils');
+
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post("/somma", function(req,res){
+  
+   res.writeHead(200, {"Content-Type": "text/html"});
+   res.write(utils.somma(req.body.A, req.body.B));
+   res.end();
+});
+
+app.post("/randpd", function(req,res){
+  
+   res.writeHead(200, {"Content-Type": "text/html"});
+   res.write(utils.randpd());
+   res.end();
+});
+
+app.post("/stringa", function(req,res){
+  
+   res.writeHead(200, {"Content-Type": "text/html"});
+   res.write(stringa(req.body.string));
+   res.end();
+});
+
+let port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
